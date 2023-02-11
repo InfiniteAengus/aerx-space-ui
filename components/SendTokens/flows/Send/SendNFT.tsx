9233237f87234f9c81fb55aeb1b4d91724796e25
Kilros0817 <lazarkitfrl@gmail.com>
2023-02-11 12:28:30 +0000

    fix popup issue when nft send

--- components/SendTokens/flows/Send/SendNFT.tsx
+++ components/SendTokens/flows/Send/SendNFT.tsx
@@ -25,7 +25,7 @@ const SendNFT: React.FC<IProps> = ({ setTransactionStatus, nfts, setSelectedNFTs
     const sendNFTs = async () => {
         try {
             if (accountId != null) {
-                setTransactionStatus("pending");
+                // setTransactionStatus("pending");
                 await nearState.nftContract?.nft_transfer({
                     receiver_id: accountId,
                     token_id: nfts[0].id,
